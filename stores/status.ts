706603df7e4a20b4dadaac3afd813d72225a3d22
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2024-12-16 23:50:56 +0800

    feat: Add section setting function

--- stores/status.ts
+++ stores/status.ts
@@ -2,10 +2,21 @@ import { defineStore } from "pinia";
 
 export const useStatusStore = defineStore("status", () => {
   const isMenuOpen = ref(false);
+  const currentSection = ref<string | null>(null);
   const isLoading = ref(false);
 
   const setIsMenuOpen = (val: boolean) => {
     isMenuOpen.value = val
+  }
+
+  const setCurrentSection = (section?: string | null) => {
+    if(section) {
+      currentSection.value = section
+      isMenuOpen.value = true
+    } else {
+      currentSection.value = null
+      isMenuOpen.value = false
+    }    
   }
 
   const setIsLoading = (val: boolean) => {
@@ -15,7 +26,9 @@ export const useStatusStore = defineStore("status", () => {
   return {
     isMenuOpen,
     isLoading,
+    currentSection,
     setIsMenuOpen,
-    setIsLoading,    
+    setIsLoading,
+    setCurrentSection, 
   };
 });
