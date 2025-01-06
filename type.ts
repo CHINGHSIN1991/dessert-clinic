ecf932ed7f00eb87854aae53aba4182852eec3aa
Ching-Hsin Chen <chinghsinchen1991@gmail.com>
2025-01-06 20:16:27 +0800

    feat: Add type declaration

--- type.ts
+++ type.ts
@@ -1,7 +1,35 @@
-export interface item {
-  id: string
-  name: string
-  
+export type ProductCategory = 'cookie' | 'b' | 'a'
+export type TimeUnit = 'year' | 'month' | 'day'
+export type ProductSpec = {
+  item_id: string,
+  spec_key: string,
+  price: number,
+  inventory: number,
+  content: {
+    name: string, 
+    qty:number
+  }[]
+}
+
+export interface product {
+  id: string,
+  enable: boolean,
+  category: ProductCategory,
+  name_key: string,
+  description_key: string,
+  preservation_key: string,
+  ingredient_keys: string[],
+  spec: ProductSpec[],
+  production_date: {
+    amount: number,
+    unit: TimeUnit
+  },
+  cover_url: string,
+  img_list: string[],
+  tags: {
+    label: string,
+    color: string,
+  }[]
 }
 
 export interface order {
